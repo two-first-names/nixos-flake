26d77287cf6a7eb63c93be8baa6cf40a2439cc35
Joe Roberts <joe@engiqueer.net>
2025-04-26 15:23:15 +0100

    Use libinput for managing touchpad

--- hosts/amity/etc.nix
+++ hosts/amity/etc.nix
@@ -1,9 +1,5 @@
 { ... }:
 {
   environment.etc = {
-    "X11/xorg.conf.d/30-touchpad.conf" = {
-      source = ./files/touchpad.conf;
-      mode = "0440";
-    };
   };
 }
--- hosts/amity/x.nix
+++ hosts/amity/x.nix
@@ -1,7 +1,11 @@
 { ... }:
 {
+  services.libinput.enable = true;
+  services.libinput.touchpad.naturalScrolling = true;
+
   services.xserver = {
     enable = true;
+    exportConfiguration = true;
     windowManager.i3.enable = true;
     displayManager.gdm.enable = true;
   };
