52a10f20efc9f97b08e31c4bacb4071ac6ddb6a5
Joe Roberts <joe@engiqueer.net>
2025-05-17 16:30:40 +0100

    Use ly as display manager

--- hosts/amity/x.nix
+++ hosts/amity/x.nix
@@ -7,8 +7,9 @@
     enable = true;
     exportConfiguration = true;
     windowManager.i3.enable = true;
-    displayManager.gdm.enable = true;
   };
+
+  services.displayManager.ly.enable = true;
 
   services.displayManager = {
     defaultSession = "none+i3";
