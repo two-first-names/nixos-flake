8015716fc8ed5f8a8478fab2204f5e175ff7b7b7
Joe Roberts <joe@engiqueer.net>
2025-05-17 16:29:58 +0100

    Add engiqueer.net TLS certificate

---
+++ hosts/amity/tls.nix
@@ -0,0 +1,21 @@
+{ ... }:
+{
+  security.pki.certificates = [
+    ''
+      engiqueer.net
+      =============
+      -----BEGIN CERTIFICATE-----
+      MIIBtDCCAVmgAwIBAgIQDLmnvF3RkngnEW4rq65f+DAKBggqhkjOPQQDAjA4MRYw
+      FAYDVQQKEw1lbmdpcXVlZXIubmV0MR4wHAYDVQQDExVlbmdpcXVlZXIubmV0IFJv
+      b3QgQ0EwHhcNMjUwMTAyMTgwNzE0WhcNMzQxMjMxMTgwNzE0WjA4MRYwFAYDVQQK
+      Ew1lbmdpcXVlZXIubmV0MR4wHAYDVQQDExVlbmdpcXVlZXIubmV0IFJvb3QgQ0Ew
+      WTATBgcqhkjOPQIBBggqhkjOPQMBBwNCAASOwXRrJpyVSmfTuHQ7Sb+PG5S+59P4
+      /+NRX5YCdOqARno0zzecsNvM+Si81Z0Ms+TcJeajsLRuT1U/RqVo3DcLo0UwQzAO
+      BgNVHQ8BAf8EBAMCAQYwEgYDVR0TAQH/BAgwBgEB/wIBATAdBgNVHQ4EFgQU9ltq
+      UiijkBQUTrsPg2BWRdeq9ZswCgYIKoZIzj0EAwIDSQAwRgIhALkQ5XkvsAAWkrfs
+      G2m+7mWvze88msXKI6sRSteoRTRVAiEA+p1E8rdMi9Su3UkWBUV/Xa6eb4SDABLL
+      ZbleGXJzjj8=
+      -----END CERTIFICATE-----
+    ''
+  ];
+}
