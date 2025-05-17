53ac42e180c1f93ed22a7a4afdb18dfbceda1316
Joe Roberts <joe@engiqueer.net>
2025-05-17 16:30:28 +0100

    Add iPhone to wifi

--- hosts/amity/wifi.nix
+++ hosts/amity/wifi.nix
@@ -8,6 +8,7 @@
       Atlantis.pskRaw = "ext:psk_atlantis";
       "Wi-Fi Guest".pskRaw = "ext:psk_2circlesquare";
       VM8264692.pskRaw = "ext:psk_2ds";
+      "iPhone".pskRaw = "ext:psk_iphone";
     };
   };
 }
